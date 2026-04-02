71060f9a6a0ccaf33059704ea6bdb5bffecd07e1
Bloknot <you@bloknotservis.ru>
2026-04-02 10:27:37 +0300

    update

--- index.js
+++ index.js
@@ -93,11 +93,6 @@ app.get('/auth/verify', (req, res) => {
 // Static files
 app.use(express.static('public'));
 
-// Catch all
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.listen(PORT, () => {
   console.log(`🚀 MINIMAL SERVER running on port ${PORT}`);
   console.log(`📊 Health: http://localhost:${PORT}/health`);
