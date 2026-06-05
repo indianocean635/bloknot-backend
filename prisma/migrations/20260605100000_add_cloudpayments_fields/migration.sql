36e7674635707945554f209df065227fc36ee1fd
Bloknot <you@bloknotservis.ru>
2026-06-05 13:39:09 +0300

    Fix migration: handle usersLimit for existing subscriptions

--- prisma/migrations/20260605100000_add_cloudpayments_fields/migration.sql
+++ prisma/migrations/20260605100000_add_cloudpayments_fields/migration.sql
@@ -6,7 +6,14 @@ ALTER TABLE "Subscription" ADD COLUMN "cloudpaymentsSubscriptionId" TEXT;
 ALTER TABLE "Subscription" ADD COLUMN "nextPaymentDate" TIMESTAMP(3);
 ALTER TABLE "Subscription" ADD COLUMN "billingPeriod" TEXT;
 ALTER TABLE "Subscription" ADD COLUMN "lastPaymentAt" TIMESTAMP(3);
-ALTER TABLE "Subscription" ADD COLUMN "usersLimit" INTEGER NOT NULL DEFAULT 1;
+ALTER TABLE "Subscription" ADD COLUMN "usersLimit" INTEGER;
+
+-- Update existing rows with default value
+UPDATE "Subscription" SET "usersLimit" = "maxUsers" WHERE "usersLimit" IS NULL;
+
+-- Make usersLimit NOT NULL
+ALTER TABLE "Subscription" ALTER COLUMN "usersLimit" SET NOT NULL;
+ALTER TABLE "Subscription" ALTER COLUMN "usersLimit" SET DEFAULT 1;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Subscription_cloudpaymentsSubscriptionId_key" ON "Subscription"("cloudpaymentsSubscriptionId");
