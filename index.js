674a711524032ba7a21bc5d7d9a943a34ca205ee
Bloknot <you@bloknotservis.ru>
2026-06-09 20:15:42 +0300

    Add CORS middleware for mobile devices

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+// CORS middleware for mobile devices
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Import missing routes
 const appointmentRoutes = require('./routes/appointmentRoutes');
 
