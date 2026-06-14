c85e03134f9517af8d25cdcaa39286cab84bff21
Bloknot <you@bloknotservis.ru>
2026-06-14 19:26:27 +0300

    Fix cloudpaymentsResponse is not defined error
    - Remove references to undefined cloudpaymentsResponse variable
    - Set cloudpaymentsSubscriptionId to null for trial subscriptions
    - Webhook will update this field when payment is confirmed

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -206,7 +206,7 @@ async function createPayment(req, res) {
           subscriptionStatus: 'TRIAL',
           trialEndsAt,
           billingPeriod: 'MONTHLY',
-          cloudpaymentsSubscriptionId: cloudpaymentsResponse.Model?.SubscriptionId || null,
+          cloudpaymentsSubscriptionId: null,
           nextPaymentDate: trialEndsAt,
           isActive: true
         },
@@ -218,7 +218,7 @@ async function createPayment(req, res) {
           subscriptionStatus: 'TRIAL',
           trialEndsAt,
           billingPeriod: 'MONTHLY',
-          cloudpaymentsSubscriptionId: cloudpaymentsResponse.Model?.SubscriptionId || null,
+          cloudpaymentsSubscriptionId: null,
           nextPaymentDate: trialEndsAt,
           isActive: true
         }
